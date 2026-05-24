1a8af1b38ec7eeefacd0b88b9a5e9d9a0b3cbeae
Andres Parra <byandrev@gmail.com>
2026-05-24 11:55:36 -0500

    feat: update Alembic env.py for SQLAlchemy configuration and add migration for users table

--- alembic/env.py
+++ alembic/env.py
@@ -5,20 +5,30 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from api import models
+from api.database import Base
+from api.config import config
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-config = context.config
+config_alembic = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+if config_alembic.config_file_name is not None:
+    fileConfig(config_alembic.config_file_name)
+
+
+config_alembic.set_main_option(
+    "sqlalchemy.url",
+    config.SQLALCHEMY_DATABASE_URI
+)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +48,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config_alembic.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -58,7 +68,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        config_alembic.get_section(config_alembic.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
